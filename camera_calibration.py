95ca8f47eb9e661c3c0052a8c3fe20a5f8117411
Ryan Cox <ryanedwincox@gmail.com>
2015-01-30 00:20:20 -0800

    modified camera_calibration to be more usable and added functionality to save and read calibration results from a file

--- camera_calibration.py
+++ camera_calibration.py
@@ -5,90 +5,134 @@ import glob
 CHESSBOARD_WIDTH = 9
 CHESSBAORD_HEIGHT = 7
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+def main():
+    # termination criteria for cornerSubPix function
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((CHESSBOARD_WIDTH*CHESSBAORD_HEIGHT,3), np.float32)
-objp[:,:2] = np.mgrid[0:CHESSBOARD_WIDTH,0:CHESSBAORD_HEIGHT].T.reshape(-1,2)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((CHESSBOARD_WIDTH*CHESSBAORD_HEIGHT,3), np.float32)
+    objp[:,:2] = np.mgrid[0:CHESSBOARD_WIDTH,0:CHESSBAORD_HEIGHT].T.reshape(-1,2)
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
 
-# Create video capture object
-cameraNum = 0;
-cap = cv2.VideoCapture(cameraNum)
+    # Create video capture object
+    cameraNum = 0;
+    cap = cv2.VideoCapture(cameraNum)
 
-# Get video properties
-w = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
-h = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+    # Get video properties
+    w = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+    h = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
 
-chessSamples = 0
+    # Initialize counter for successful samples
+    chessSamples = 0
 
-#Collect imgpoints
-while cap.isOpened():
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    #Collect imgpoints
+    while cap.isOpened():
+        # Read image and convert to grayscale for processing
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (CHESSBOARD_WIDTH,CHESSBAORD_HEIGHT),None)
+        # Find chessboard corners
+        ret, corners = cv2.findChessboardCorners(gray, (CHESSBOARD_WIDTH,CHESSBAORD_HEIGHT),None)
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        chessSamples = chessSamples + 1
-        print str(chessSamples)
-    
-        objpoints.append(objp)
-
-        cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners)
-
-        # Draw and display the corners
-        cv2.drawChessboardCorners(img, (CHESSBOARD_WIDTH,CHESSBAORD_HEIGHT), corners, ret)
-    
-    cv2.imshow('img',img)
-
-    # Quit on 'q' press
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
-
-# calibrate camera
-print "calibrating"
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-print "finished"
-
-# mtx = np.matrix('927.85,0,385.35;0,813.87,4.322;0,0,1')
-# dist = np.matrix('0.203, -0.0713, -0.054324, 0.013143, 0.007245')
-
-print "mtx" + str(mtx)
-print "dist" + str(dist)
-# # print "rvecs" + str(rvecs)
-# # print "tvecs" + str(tvecs)
-
-
-
-# # Test calibration
-# while cap.isOpened():
-
-    # ret, img = cap.read()
-
-    # # Undistort video
-    # newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-    # dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
-
-    # # w,h,d = dst.shape
-    # # print str(w) + " " + str(h)
-    
-    # # crop the image
-    # x,y,w,h = roi
-    # newImg = dst[y:y+h, x:x+w]
-
-    # cv2.imshow('img',dst)
-    
-    # # Quit on 'q' press
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-        # break
+        # If chessboard corners found, add object points, image points (after refining them)
+        if ret == True:
+            # Increment counter for successful samples
+            chessSamples = chessSamples + 1
         
-# cv2.destroyAllWindows()
+            # Include fewer samples in calibration calculation to avoid excessive computation time
+            if chessSamples % 5 == 0:
+                print str(chessSamples/5)
+                
+                # Add chessboard points measured in the world
+                objpoints.append(objp)
+
+                # Refines corner detection
+                cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+                
+                # Add detected corners pixel location
+                imgpoints.append(corners)
+
+            # Draw and display the corners
+            cv2.drawChessboardCorners(img, (CHESSBOARD_WIDTH,CHESSBAORD_HEIGHT), corners, ret)
+
+        # Display the video feed
+        cv2.imshow('img',img)
+
+        # Limit number of samples accumulated to avoid excessive computation times
+        if chessSamples/5 > 50:
+            break
+        
+        # Quit on 'q' press
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+    # calibrate camera
+    print "calibrating"
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+    print "finished\n"
+    
+    # Write calibration results to file so they can be retrieved easy later
+    write_to_file(mtx, dist)
+    
+    # Read calibration results from file
+    mxt, dist = read_from_file()
+    
+
+    # # Test calibration
+    # while cap.isOpened():
+
+        # ret, img = cap.read()
+
+        # # Undistort video
+        # newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+        # dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+        # # w,h,d = dst.shape
+        # # print str(w) + " " + str(h)
+        
+        # # crop the image
+        # x,y,w,h = roi
+        # newImg = dst[y:y+h, x:x+w]
+
+        # cv2.imshow('img',dst)
+        
+        # # Quit on 'q' press
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+            # break
+            
+    # cv2.destroyAllWindows()
+
+# Write camera calibration data to a file
+def write_to_file (mtx, dist):
+    # Open a file is write mode
+    f = open('cam_cal.txt', 'w')
+    
+    # Write mtx and dist matrix
+    np.savez(f, mtx, dist)
+    
+    # Close file so it can be read from later
+    f.close()
+    
+    print "calibration results written to file cam_cal.txt\n"
+
+# Read camera calibration read from file
+def read_from_file ():
+    # Open file to read from
+    f = open('cam_cal.txt', 'r')
+    
+    # Read file
+    npzfile = np.load(f)
+    mtx = npzfile['arr_0']
+    dist = npzfile['arr_1']
+    
+    # Close file
+    f.close()
+    
+    return mtx, dist
+    
+# Run program    
+main()
